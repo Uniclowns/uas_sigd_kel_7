bcecef1b21b4d0d849b049550966c6aa3f667e40
Pheterson Ferry Fernando <phetersonnnz@gmail.com>
2024-05-26 20:35:55 +0700

    Update index.blade.php

--- resources/views/maps/index.blade.php
+++ resources/views/maps/index.blade.php
@@ -8,7 +8,6 @@
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- leaflet.js file-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/leaflet@1.7.1/dist/leaflet.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet.draw/1.0.4/leaflet.draw.css">
     <style>
         #map {
             height: 100vh;
@@ -19,13 +18,18 @@
 <body>
     <div id="map"></div>
 
+    <div class="relative">
+        <a href="{{ route('maps.create') }}">
+            Tambah Data
+        </a>
+    </div>
+
     {{-- Bootstrap --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
     <!-- leaflet.js file-->
     <script src="https://cdn.jsdelivr.net/npm/leaflet@1.7.1/dist/leaflet.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet.draw/1.0.4/leaflet.draw.js"></script>
     <script>
         var map =
             L.map('map').setView([-
