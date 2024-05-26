daa923c7848a9430e8826a1e91e2f378fce17d75
Pheterson Ferry Fernando <phetersonnnz@gmail.com>
2024-05-26 23:01:54 +0700

    Update index.blade.php

--- resources/views/maps/index.blade.php
+++ resources/views/maps/index.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 @section('container')
-
-
-
+    <div class="flex items-center justify-center">
+        <div class="mx-12 ">
+            <h1 class="text-3xl font-bold text-center">Google Maps</h1>
+        </div>
+        <div id="map"></div>
+    </div>
 @endsection
