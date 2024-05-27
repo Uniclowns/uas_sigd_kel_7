1b4098339248b30620b407f211c2cdb73a9c1121
Pheterson Ferry Fernando <phetersonnnz@gmail.com>
2024-05-27 15:50:00 +0700

    Update

--- resources/views/maps/menu.blade.php
+++ resources/views/maps/menu.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.app')
 @section('container')
-    <div class="container bg-[#DDE4FF] h-[200vh]">
+    <div class="container h-100">
         <div class="py-8 text-base">
-            <span><a href="/dashboard" class="text-[#5C59E8]">Beranda</a> > Kelola Driver</span>
+            <span><a href="/dashboard" class="text-[#5C59E8]">Beranda</a> > Kelola Pemetaan Fakultas Universitas
+                Tanjungpura</span>
         </div>
 
         @if (session()->has('success'))
@@ -13,70 +14,56 @@
 
         <div class="flex justify-between items-center">
             <div class="w-[50%] ">
-                <form action="/driver">
+                <form action="/maps">
                     <input type="text" name="search" id="search" placeholder="🔍   Cari"
-                           class="block w-full bg-[#F6F6F6] rounded-full px-8 py-2 focus:ring-[##4880FF] focus:border-[##4880FF] text-[#202224] text-sm border border-[#D5D5D5] placeholder-[#202224]">
+                        class="block w-full rounded-full px-8 py-2 text-sm">
                 </form>
-            </div>
-
-            <div class="bg-primary rounded-xl">
-                <a class="flex items-center p-2" href="/driver/create">
-                    <img src="/img/plus.png" > <span class="text-white font-sans font-bold"> Terima Driver</span>
-                </a>
             </div>
         </div>
 
         <div class="p-2 bg-white mt-4 rounded-xl w-full py-64 mb-32">
             <table class="items-center w-full mb-0 align-top">
                 <thead>
-                <tr>
-                    <th class="px-6 py-3 pl-2 font-bold text-center border-b text-sm">No</th>
-                    <th class="px-6 py-3 pl-2 font-bold text-center  border-b text-sm">Nama</th>
-                    <th class="px-6 py-3 pl-2 font-bold text-center  border-b text-sm">Alamat</th>
-                    <th class="px-6 py-3 pl-2 font-bold text-center  border-b text-sm">No. Handphone</th>
-                    <th class="px-6 py-3 pl-2 font-bold text-center  border-b text-sm">Foto Profil</th>
-                    <th class="px-6 py-3 pl-2 font-bold text-center  border-b text-sm">Plat Kendaraan</th>
-                    <th class="px-6 py-3 pl-2 font-bold text-center  border-b text-sm">Aksi</th>
-                </tr>
+                    <tr>
+                        <th class="px-6 py-3 pl-2 font-bold text-center border-b text-sm">No</th>
+                        <th class="px-6 py-3 pl-2 font-bold text-center  border-b text-sm">Name</th>
+                        <th class="px-6 py-3 pl-2 font-bold text-center  border-b text-sm">Latitude</th>
+                        <th class="px-6 py-3 pl-2 font-bold text-center  border-b text-sm">Longitude</th>
+                        <th class="px-6 py-3 pl-2 font-bold text-center  border-b text-sm">Action</th>
+                    </tr>
                 </thead>
                 <tbody>
-                @foreach($places as $place)
-                    <tr class="border-b">
-                        <td class="p-2 text-center">{{ $loop->iteration }}</td>
-                        <td class="p-2 font-semibold">{{ $place->nama }}</td>
-                        <td class="p-2 font-semibold">{{ $place->alamat }}</td>
-                        <td class="p-2 font-semibold text-center">{{ $place->no_hp }}</td>
-                        <td class="p-2 font-semibold text-center">{{ $place->foto_profil }}</td>
-                        <td class="p-2 font-semibold text-center">{{ $place->plat_kendaraan }}</td>
-                        <td class="p-2 font-semibold text-center">
-                            <div class="flex">
-                                <div class="bg-[#4AD989] flex justify-center px-2 py-0.5 rounded-lg mx-2 items-center">
-                                    <a href="/driver/{{ $driver->id }}/edit" class="bg-[#4AD989] shadow-xl">
-                                        <img src="/img/edit.png" alt="">
-                                    </a>
+                    @foreach ($places as $place)
+                        <tr class="border-b">
+                            <td class="p-2 text-center">{{ $loop->iteration }}</td>
+                            <td class="p-2 font-semibold">{{ $place->name }}</td>
+                            <td class="p-2 font-semibold">{{ $place->name }}</td>
+                            <td class="p-2 font-semibold">{{ $place->name }}</td>
+                            <td class="p-2 font-semibold text-center">
+                                <div class="flex">
+                                    <div class="bg-[#4AD989] flex justify-center px-2 py-0.5 rounded-lg mx-2 items-center">
+                                        <a href="/place/{{ $place->id }}/edit" class="bg-[#4AD989] shadow-xl">
+                                            <img src="/img/edit.png" alt="">
+                                        </a>
+                                    </div>
+                                    <div class="bg-red-500 flex justify-center px-2 py-0.5 rounded-xl mx-2 items-center">
+                                        <form action="{{ route('place.destroy', $place->id) }}" method="post">
+                                            @method('delete')
+                                            @csrf
+                                            <button class="bg-[#FF0000]"
+                                                onclick="confirm('Apa kamu yakin ingin menghapus place berikut ?')">
+                                                <img src="/img/delete.png" alt="">
+                                            </button>
+                                        </form>
+                                    </div>
                                 </div>
-                                <div class="bg-red-500 flex justify-center px-2 py-0.5 rounded-xl mx-2 items-center">
-                                    <form action="{{ route('driver.destroy', $driver->id) }}" method="post">
-                                        @method('delete')
-                                        @csrf
-                                        <button class="bg-[#FF0000]" onclick="confirm('Apa kamu yakin ingin menghapus driver berikut ?')">
-                                            <img src="/img/delete.png" alt="">
-                                        </button>
-                                    </form>
-                                </div>
-                                <div class="flex justify-center px-2 py-0.5 rounded-xl items-center" style="background-color: #FCBE2D;">
-                                    <a href="/driver/{{ $driver->id }}" class="shadow-xl bg-yellow-400" style="background-color: #FCBE2D;">
-                                        <img src="/img/show.png" alt="">
-                                    </a>
-                                </div>
-                            </div>
-                        </td>
-                    </tr>
-                @endforeach
+                            </td>
+                        </tr>
+                    @endforeach
                 </tbody>
             </table>
             <div class="mt-4 flex mx-4">
-                {{ $drivers->links() }}
+                {{ $places->links() }}
             </div>
         </div>
     </div>
